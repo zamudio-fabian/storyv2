d26244cec34d4695371d420f0a59a1558a22ed2e
Fabian Zamudio <fabian.zamudio.89@gmail.com>
2023-08-09 21:49:11 -0300

    change names

--- projects/components/qr-ui-library/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of qr-ui-library
- */
-
-
-export * from './lib/qr-ui-library.module';
-export * from './lib/components/qr-button/qr-button.component';
-export * from './lib/components/qr-button-custom/qr-button-custom.component';
